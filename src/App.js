02de771e5e2b710b63300d7b0cfe78d6a62bde43
vitoria kelly <vitoriacostal@alu.ufc.br>
2024-05-20 22:29:41 -0300

    build

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        
-      <Route path="/" element={<Login />} />
+      <Route path="/my-list" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<TaskCategory />} />
 
